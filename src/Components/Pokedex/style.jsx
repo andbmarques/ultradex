4538f59e48c01b06469edd0e3d5abebd038e8a03
Anderson B <andbmarques@gmail.com>
2023-01-06 12:48:58 -0300

    :x: Remove: Select Texts in Pokedex

--- src/Components/Pokedex/style.jsx
+++ src/Components/Pokedex/style.jsx
@@ -1,43 +1,49 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 70vh;
-    padding: 2rem;
+  height: 70vh;
+  padding: 2rem;
 `;
 
 export const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.Charcoal };
-    padding: 1rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.Charcoal};
+  padding: 1rem 0;
 `;
 
 export const Title = styled.h1`
-    color: ${({ theme }) => theme.colors.Charcoal };
+  color: ${({ theme }) => theme.colors.Charcoal};
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
 `;
 
 export const Text = styled.p`
-
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
 `;
 
-export const MainContainer = styled.div`
-
-`;
+export const MainContainer = styled.div``;
 
 export const PokemonListContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    padding: 1rem;
-    flex-wrap: wrap;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  flex-wrap: wrap;
+  justify-content: center;
 
-    @media screen and (max-width: 740px) {
-        flex-direction: column;
-        align-items: center;
-    }
+  @media screen and (max-width: 740px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const LoadingText = styled.p`
-    margin: 1rem 0;
+  margin: 1rem 0;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
 `;
